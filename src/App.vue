e963ec476e79caeb35a3d00afcb17c0e8038ccb8
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-06-04 15:35:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+import { store } from './components/data/store'
+
+</script>
+
+<template>
+  <AppHeader />
+  
+  <AppMain />
+  
+  <AppFooter />
+  
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,49 @@
+<script>
+import { store } from '/src/components/data/store'
+
+
+
+export default {
+    components: {
+        AppMain
+    },
+    data() {
+        return {
+            store
+
+        }
+    },
+    methods: {
+        getImage(nomefile) {
+            return new URL(`../assets/${nomefile}, import.meta.url`);
+            },
+        },
+        prevImage() {
+            this.store.posts--
+            if (this.store.posts < 0)
+                this.store.posts = 2
+            console.log(this.store.img[this.data.posts].badge);
+        },
+        nextImage() {
+            this.store.posts++
+            if (this.store.posts> 2)
+                this.store.posts = 0
+            console.log(this.store.posts[this.data.posts].badge);
+        },
+        visible(i) {
+            if (i <= this.store.posts + 3 && i >= this.store.posts) {
+                return true
+            }
+        },
+
+        
+        mounted() {
+        console.log(this.store.posts)
+        }
+    }
+    
+
+</script>
+
+<!-- this.store.cards[this.store.currentCards].path -->
+
