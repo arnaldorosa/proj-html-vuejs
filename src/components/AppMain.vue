51f59d68e01e0f73e62b6f4e926f7976a911a9ed
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-06-06 11:44:51 +0200

    end exercise

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,49 +1,34 @@
 <script>
-import { store } from '/src/components/data/store'
-
-
+import store from '../data/store.js';
+import CardGrid from './CardGrid.vue';
+import PostList from './PostList.vue';
+import MainBanner from './MainBanner.vue';
 
 export default {
-    components: {
-        AppMain
-    },
-    data() {
-        return {
-            store
-
-        }
-    },
-    methods: {
-        getImage(nomefile) {
-            return new URL(`../assets/${nomefile}, import.meta.url`);
-            },
-        },
-        prevImage() {
-            this.store.posts--
-            if (this.store.posts < 0)
-                this.store.posts = 2
-            console.log(this.store.img[this.data.posts].badge);
-        },
-        nextImage() {
-            this.store.posts++
-            if (this.store.posts> 2)
-                this.store.posts = 0
-            console.log(this.store.posts[this.data.posts].badge);
-        },
-        visible(i) {
-            if (i <= this.store.posts + 3 && i >= this.store.posts) {
-                return true
-            }
-        },
-
-        
-        mounted() {
-        console.log(this.store.posts)
-        }
-    }
-    
-
+  components: {
+    CardGrid,
+    PostList,
+    MainBanner,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {},
+  mounted() {},
+};
 </script>
-
-<!-- this.store.cards[this.store.currentCards].path -->
-
+<template>
+  <section class="container-nobootstrap">
+    <CardGrid />
+    <PostList />
+  </section>
+  <MainBanner />
+</template>
+<style scoped>
+.container-nobootstrap {
+  width: 1400px;
+  margin: 0 auto;
+}
+</style>
